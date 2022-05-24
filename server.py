48d45fd9c8a4539ef25b5af8ac40518135fceef3
wisang <wseom7@gmail.com>
2022-05-24 14:57:51 +0900

    decorator exercises

--- server.py
+++ server.py
@@ -5,12 +5,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_flask():
-    return "Hello, Flask!"
+    return "<h1>Hello, Flask!</h1>" \
+           "<p>paragraph</p>"
 
+def make_bold(func):
+    def wrapper_function():
+        return f'<b>{func()}</b>'
+    return wrapper_function
 
-@app.route('/<name>/<age>')
-def bye_flask(name, age):
-    return f"hello, there {name}, you are {age} years old"
+def make_emphasis(func):
+    def wrapper_function():
+        return f'<em>{func()}</em>'
+    return wrapper_function
+
+def make_underline(func):
+    def wrapper_function():
+        return f'<u>{func()}</u>'
+    return wrapper_function
+
+@app.route('/test')
+@make_bold
+@make_emphasis
+@make_underline
+def decorator_test():
+    return '<h1 style="text-align: center">test<h1>'
+
+@app.route('/<name>/<int:number>')
+def bye_flask(name, number):
+    return f"hello, there {name}, you are {number} years old"
 
 
 if __name__ == '__main__':
